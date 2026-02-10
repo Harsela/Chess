da96ef50827e742d1ff2d03cd136fe535b251570
harsela s <harselakavitha841@gmail.com>
2026-02-10 19:03:11 +0530

    file addition

---
+++ chess/study/MultiCatch.java
@@ -0,0 +1,20 @@
+package chess.study;
+
+public class MultiCatch {
+    public static void main(String[] args) {
+        try {
+            int a[] = new int[5];
+            a[6] = 10 / 0;
+        } catch (ArithmeticException e) {
+            System.out.println("A-Cannot divide by 0");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Index out of bound");
+        }
+        try{
+            int b[]=new int[3];
+            b[2]=10/0;
+        }catch(Exception e){
+            System.out.println("B-Cannot divide by 0");
+        }
+    }
+}
